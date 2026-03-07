43205bc8b4f09850583e05dba8dede862945534d
varshapm02 <pmvarsha02@gmail.com>
2026-03-07 21:29:17 +0530

    main and pom.xml

---
+++ src/main/java/com/example/taskmaster_backend/TaskmasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmaster_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterBackendApplication.class, args);
+	}
+
+}
